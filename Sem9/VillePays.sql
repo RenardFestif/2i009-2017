a838b9f7430adcbb5eb93a149eac5c9ed3f3db52
SAADI JUBA <3520173@ppti-14-302-11.ufr-info-p6.jussieu.fr>
2017-03-27 17:48:38 +0200

    pas grand chose

--- Sem9/VillePays.sql
+++ Sem9/VillePays.sql
@@ -1,17 +1,18 @@
 create table Ville(
-  nom varchar,
+  nom varchar(20),
   population number,
-  pays varchar,
+  pays varchar(20),
   
-  constraint fk_pays foreign key (pays) references Pays (nom)
+  constraint fk_pays foreign key (pays) references Pays
   );
   
 create table Pays(
-  nom varchar, 
-  capitale varchar,
+  nom varchar(20), 
+  capitale varchar(20),
   
   constraint pk_nom primary key (nom),
   constraint fk-cap foreign key (capitale) references Ville (nom)
   );
   
-  
+insert into Ville values ('Paris',3000000,'France');
+insert into Pays values ('France','Paris');
